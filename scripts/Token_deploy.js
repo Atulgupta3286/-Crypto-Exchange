2553fc2d45da39f2b7b0b0e4a8b114beda7e7149
Atul Gupta <princegupta93022gmail.com>
2022-11-18 19:34:56 +0530

    first commit

---
+++ scripts/Token_deploy.js
@@ -0,0 +1,18 @@
+const { ethers } = require("hardhat");
+const hre = require("hardhat");
+
+async function main() {
+    const Token = await ethers.getContractFactory("Token");
+    const token = await Token.deploy();
+    await token.deployed
+    console.log(`smart contract deployed ${token.address}` );
+}
+
+
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
